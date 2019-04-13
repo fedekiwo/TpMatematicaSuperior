1a8c465dcf1d857a08cdc80414bc363ea04d734b
fedekiwo <fedekiwo@gmail.com>
2019-04-13 17:34:41 -0300

    Clase Complex, operaciones suma/resta/multiplicación definidas.

---
+++ TpMatematicaSuperior/Model/ComplexNumbers/Complex.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpMatematicaSuperior.Model.ComplexNumbers
+{
+    public class Complex
+    {
+        public Decimal Imaginary { get; set; }
+        public Decimal Real { get; set; }
+
+        public Complex(Decimal Real, Decimal Imaginary) {
+            this.Real = Real;
+            this.Imaginary = Imaginary;
+        }
+
+        public string GetNumber() {
+            return this.Real.ToString() + " " + this.Imaginary.ToString() +  "j";
+        }
+
+        private Complex GetMyConjugate() {
+            return new Complex(this.Real, - this.Imaginary);
+        }
+
+        private Complex GetMyOpposite()
+        {
+            return new Complex(-this.Real, -this.Imaginary);
+        }
+
+        public static Complex operator +(Complex firstComplex, Complex secondComplex) {
+            return new Complex(firstComplex.Real + secondComplex.Real, 
+                               firstComplex.Imaginary + secondComplex.Imaginary);
+        }
+        public static Complex operator -(Complex firstComplex, Complex secondComplex) {
+            return firstComplex + secondComplex.GetMyOpposite();
+        }
+
+        public static Complex operator *(Complex firstComplex, Complex secondComplex) {
+            return new Complex(firstComplex.Real * secondComplex.Real,
+                               firstComplex.Imaginary * secondComplex.Imaginary);
+        }
+
+    }
+}
---
+++ TpMatematicaSuperior/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TpMatematicaSuperior.Model.ComplexNumbers;
+
+namespace TpMatematicaSuperior
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Complex n1 = new Complex(-1, 3);
+            Complex n2 = new Complex(2, -5);
+
+            var sum = n1 + n2;
+            var subs = n1 - n2;
+            var mult = n1 * n2;
+            Console.WriteLine("Suma: " + sum.GetNumber());
+            Console.WriteLine("Resta: " + subs.GetNumber());
+            Console.WriteLine("Mult: " + mult.GetNumber());
+            Console.WriteLine(Math.PI);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+    }
+}
